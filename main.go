69ad2fdd1f130b6801cfbb992ca638a438d153ce
Erik Vattekar <erik.vattekar@nav.no>
2025-05-07 10:05:18 +0200

    alert is log based

--- main.go
+++ main.go
@@ -17,8 +17,6 @@ var caBundlePath, slackToken, slackChannel string
 
 func main() {
 	flag.StringVar(&caBundlePath, "ca-bundle-path", os.Getenv("CA_BUNDLE_PATH"), "Path to the ca bundle file")
-	flag.StringVar(&slackToken, "slack-token", os.Getenv("SLACK_TOKEN"), "The slack token")
-	flag.StringVar(&slackChannel, "slack-channel-id", os.Getenv("SLACK_CHANNEL_ID"), "The slack channel ID for certificate expiration notifications")
 	flag.Parse()
 
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
@@ -44,35 +42,14 @@ func main() {
 		}
 
 		if expiredOrExpiresWithinAWeek(cert) {
-			if err := notifyCertExpires(cert, slackClient, logger); err != nil {
-				logger.Error("notifying certificate expired", "error", err)
-			}
+      logger.Error(fmt.Sprintf(
+        "Issuer: %v, Subject: %v, NotAfter: %v",
+        cert.Issuer, cert.Subject, cert.NotAfter
+      ))
 		}
 	}
 }
 
 func expiredOrExpiresWithinAWeek(cert *x509.Certificate) bool {
 	return math.Floor(cert.NotAfter.Sub(time.Now()).Hours()/24) < 7
-}
-
-func notifyCertExpires(cert *x509.Certificate, slackClient *slack.Client, logger *slog.Logger) error {
-	logger.Info("sending slack notification regarding expiring certificate",
-		"Issuer", cert.Issuer,
-		"Subject", cert.Subject,
-		"NotAfter", cert.NotAfter.String(),
-	)
-
-	_, _, err := slackClient.PostMessage(slackChannel, slack.MsgOptionAttachments(slack.Attachment{
-		Title: ":warning: Certificate expired or expiring within a week",
-		Color: "yellow",
-		Text: fmt.Sprintf(
-			"Issuer: %v, Subject: %v, NotAfter: %v",
-			cert.Issuer, cert.Subject, cert.NotAfter,
-		),
-	}))
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
